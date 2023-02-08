8b2b573d26041548c83ffee76a80ed669ac00edb
massil teklal <teklalmassil2000@gmail.com>
2023-02-08 23:36:47 +0100

    3eme depot

---
+++ Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace WeatherApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.labelCondition = new System.Windows.Forms.Label();
+            this.labelDetails = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.labelSunrise = new System.Windows.Forms.Label();
+            this.labelSunset = new System.Windows.Forms.Label();
+            this.labelPressure = new System.Windows.Forms.Label();
+            this.labelWindSpeed = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.WindSpeed = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.labelTemperature = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(114, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "City:";
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCity.Location = new System.Drawing.Point(199, 72);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(183, 36);
+            this.textBoxCity.TabIndex = 1;
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.Color.DarkGray;
+            this.btn.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.ForeColor = System.Drawing.Color.White;
+            this.btn.Location = new System.Drawing.Point(576, 62);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(107, 46);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "Search";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // labelCondition
+            // 
+            this.labelCondition.AutoSize = true;
+            this.labelCondition.BackColor = System.Drawing.Color.Transparent;
+            this.labelCondition.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCondition.ForeColor = System.Drawing.Color.White;
+            this.labelCondition.Location = new System.Drawing.Point(115, 255);
+            this.labelCondition.Name = "labelCondition";
+            this.labelCondition.Size = new System.Drawing.Size(110, 29);
+            this.labelCondition.TabIndex = 3;
+            this.labelCondition.Text = "Condition";
+            // 
+            // labelDetails
+            // 
+            this.labelDetails.AutoSize = true;
+            this.labelDetails.BackColor = System.Drawing.Color.Transparent;
+            this.labelDetails.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDetails.ForeColor = System.Drawing.Color.White;
+            this.labelDetails.Location = new System.Drawing.Point(115, 296);
+            this.labelDetails.Name = "labelDetails";
+            this.labelDetails.Size = new System.Drawing.Size(81, 29);
+            this.labelDetails.TabIndex = 4;
+            this.labelDetails.Text = "Details";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(115, 339);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 29);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Sunrise:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(115, 383);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 29);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Sunset:";
+            // 
+            // labelSunrise
+            // 
+            this.labelSunrise.AutoSize = true;
+            this.labelSunrise.BackColor = System.Drawing.Color.Transparent;
+            this.labelSunrise.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSunrise.ForeColor = System.Drawing.Color.White;
+            this.labelSunrise.Location = new System.Drawing.Point(239, 339);
+            this.labelSunrise.Name = "labelSunrise";
+            this.labelSunrise.Size = new System.Drawing.Size(51, 29);
+            this.labelSunrise.TabIndex = 7;
+            this.labelSunrise.Text = "N/A";
+            // 
+            // labelSunset
+            // 
+            this.labelSunset.AutoSize = true;
+            this.labelSunset.BackColor = System.Drawing.Color.Transparent;
+            this.labelSunset.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSunset.ForeColor = System.Drawing.Color.White;
+            this.labelSunset.Location = new System.Drawing.Point(239, 383);
+            this.labelSunset.Name = "labelSunset";
+            this.labelSunset.Size = new System.Drawing.Size(57, 29);
+            this.labelSunset.TabIndex = 8;
+            this.labelSunset.Text = "City:";
+            // 
+            // labelPressure
+            // 
+            this.labelPressure.AutoSize = true;
+            this.labelPressure.BackColor = System.Drawing.Color.Transparent;
+            this.labelPressure.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPressure.ForeColor = System.Drawing.Color.White;
+            this.labelPressure.Location = new System.Drawing.Point(632, 296);
+            this.labelPressure.Name = "labelPressure";
+            this.labelPressure.Size = new System.Drawing.Size(51, 29);
+            this.labelPressure.TabIndex = 12;
+            this.labelPressure.Text = "N/A";
+            // 
+            // labelWindSpeed
+            // 
+            this.labelWindSpeed.AutoSize = true;
+            this.labelWindSpeed.BackColor = System.Drawing.Color.Transparent;
+            this.labelWindSpeed.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWindSpeed.ForeColor = System.Drawing.Color.White;
+            this.labelWindSpeed.Location = new System.Drawing.Point(632, 252);
+            this.labelWindSpeed.Name = "labelWindSpeed";
+            this.labelWindSpeed.Size = new System.Drawing.Size(51, 29);
+            this.labelWindSpeed.TabIndex = 11;
+            this.labelWindSpeed.Text = "N/A";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.ForeColor = System.Drawing.Color.White;
+            this.label15.Location = new System.Drawing.Point(466, 296);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(102, 29);
+            this.label15.TabIndex = 10;
+            this.label15.Text = "Pressure:";
+            // 
+            // WindSpeed
+            // 
+            this.WindSpeed.AutoSize = true;
+            this.WindSpeed.BackColor = System.Drawing.Color.Transparent;
+            this.WindSpeed.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WindSpeed.ForeColor = System.Drawing.Color.White;
+            this.WindSpeed.Location = new System.Drawing.Point(466, 252);
+            this.WindSpeed.Name = "WindSpeed";
+            this.WindSpeed.Size = new System.Drawing.Size(138, 29);
+            this.WindSpeed.TabIndex = 9;
+            this.WindSpeed.Text = "Wind Speed:";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.Color.Transparent;
+            this.label17.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.ForeColor = System.Drawing.Color.White;
+            this.label17.Location = new System.Drawing.Point(466, 339);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(146, 29);
+            this.label17.TabIndex = 13;
+            this.label17.Text = "Temperature:";
+            // 
+            // labelTemperature
+            // 
+            this.labelTemperature.AutoSize = true;
+            this.labelTemperature.BackColor = System.Drawing.Color.Transparent;
+            this.labelTemperature.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTemperature.ForeColor = System.Drawing.Color.White;
+            this.labelTemperature.Location = new System.Drawing.Point(632, 339);
+            this.labelTemperature.Name = "labelTemperature";
+            this.labelTemperature.Size = new System.Drawing.Size(51, 29);
+            this.labelTemperature.TabIndex = 14;
+            this.labelTemperature.Text = "N/A";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox.Location = new System.Drawing.Point(119, 145);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(106, 80);
+            this.pictureBox.TabIndex = 15;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(878, 515);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.labelTemperature);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.labelPressure);
+            this.Controls.Add(this.labelWindSpeed);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.WindSpeed);
+            this.Controls.Add(this.labelSunset);
+            this.Controls.Add(this.labelSunrise);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.labelDetails);
+            this.Controls.Add(this.labelCondition);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.label2);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxVille;
+        private System.Windows.Forms.Label labCondition;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label labDetails;
+        private System.Windows.Forms.Label Sunrise;
+        private System.Windows.Forms.Label labSunrise;
+        private System.Windows.Forms.Label labSunset;
+        private System.Windows.Forms.Label Sunset;
+        private System.Windows.Forms.Label labPressure;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labWindSpeed;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labTemperature;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox picIcon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label labelCondition;
+        private System.Windows.Forms.Label labelDetails;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labelSunrise;
+        private System.Windows.Forms.Label labelSunset;
+        private System.Windows.Forms.Label labelPressure;
+        private System.Windows.Forms.Label labelWindSpeed;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label WindSpeed;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label labelTemperature;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace WeatherApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string APIKey = "2d1b7fbfc17441146795a0a0c78e5ac6";
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            getWeather();
+        }
+        void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", textBoxCity.Text, APIKey);
+                var json = web.DownloadString(url);
+                WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+                pictureBox.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                labelCondition.Text = Info.weather[0].main;
+                labelDetails.Text = Info.weather[0].description;
+
+                labelSunset.Text = convertDateTime(Info.sys.sunset).ToShortTimeString();
+                labelSunrise.Text = convertDateTime(Info.sys.sunrise).ToShortTimeString();
+
+                labelWindSpeed.Text = Info.wind.speed.ToString();
+                labelPressure.Text = Info.main.pressure.ToString();
+                int tpInt = (int)((Info.main.temp) - 273.15);
+
+                if (tpInt == 1)
+                {
+
+                    labelTemperature.Text = "Alerte Canicule (" + ((int)((Info.main.temp) - 273.15)) + "°)";
+                }
+                else
+                {
+                    labelTemperature.Text = ((int)((Info.main.temp) - 273.15)).ToString();
+                }
+
+                //labPREF1.Text = textBoxVille.Text;
+
+            }
+        }
+        DateTime convertDateTime(long sec)
+        {
+            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).ToLocalTime();
+            day = day.AddSeconds(sec).ToLocalTime();
+
+            return day;
+        }
+
+        private void textBoxCity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
